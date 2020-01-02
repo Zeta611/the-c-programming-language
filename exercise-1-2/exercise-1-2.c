bbc16951daca18b310a1164c632d2b30fc9cdd07
Jaeho Lee <jaeho.lee@snu.ac.kr>
2020-01-02 10:17:48 +0900

    :sparkles: Added Exercise 1-2

---
+++ exercise-1-2/exercise-1-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main()
+{
+	printf("Alert              : '\a'\n");
+	printf("Backspace          : '\b'\n");
+	printf("Escape character   : '\e'\n");
+	printf("Formfeed page break: '\e'\n");
+	printf("Newline            : '\n'\n");
+	printf("Carriage return    : '\r'\n");
+	printf("Horizontal tab     : '\t'\n");
+	printf("Vertical tab       : '\v'\n");
+	printf("Backslash          : '\\'\n");
+	printf("Apostrophe         : '\''\n");
+	printf("Double quotation   : '\"'\n");
+	printf("Question mark      : '\?'\n");
+}
